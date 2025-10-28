19c885531a6eccd9b87118bdf7d032289fca5333
Cristian Villalba <villalba.macri24@triana.salesianos.edu>
2025-10-28 14:29:06 +0100

    Proyecto Spring iniciado

---
+++ CristianVillalbaBiblioteca/src/main/java/com/salesianostriana/dam/cristianvillalbabiblioteca/CristianVillalbaBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.cristianvillalbabiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CristianVillalbaBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CristianVillalbaBibliotecaApplication.class, args);
+	}
+
+}
---
+++ CristianVillalbaBiblioteca/src/test/java/com/salesianostriana/dam/cristianvillalbabiblioteca/CristianVillalbaBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.cristianvillalbabiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CristianVillalbaBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
