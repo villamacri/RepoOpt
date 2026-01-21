2fa457657ae2ee08227aac91b64ed62feae9e0a7
villamacri <villalba.macri24@triana.salesianos.edu>
2026-01-21 16:21:54 +0100

    ejercicio05 incompleto

--- EjerciciosUD04CristianVillalba/src/ejercicio05/Cuenta.java
+++ EjerciciosUD04CristianVillalba/src/ejercicio05/Cuenta.java
@@ -2,4 +2,6 @@ package ejercicio05;
 
 public class Cuenta {
 
+    private int id;
+    private double saldo;
 }
--- EjerciciosUD04CristianVillalba/src/ejercicio05/Operaciones.java
+++ EjerciciosUD04CristianVillalba/src/ejercicio05/Operaciones.java
@@ -1,5 +1,25 @@
 package ejercicio05;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class Operaciones {
 
+    private final HashSet<Integer>listaNumeros;
+
+    public Operaciones(HashSet<Integer> listaNumeros) {
+        this.listaNumeros = listaNumeros;
+    }
+
+    public int generarNumero() throws NumeroRepetidoException{
+        Random rnd=new Random();
+
+        int max=100, min=1, num=rnd.nextInt(max-min+1)+min;
+
+        if(!listaNumeros.add(num)){
+            throw new NumeroRepetidoException("El número " + num + " ya ha salido");
+        }
+        return num;
+    }
 }
