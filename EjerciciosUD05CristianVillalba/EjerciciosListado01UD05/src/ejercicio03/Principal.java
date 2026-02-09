37deab08c7a46e047b20f77a4693b0f5b20be419
Cristian Villalba <villalba.macri24@triana.salesianos.edu>
2026-02-09 13:40:43 +0100

    ejerciciosFicheros 03 hecho

--- EjerciciosUD05CristianVillalba/EjerciciosListado01UD05/src/ejercicio03/Principal.java
+++ EjerciciosUD05CristianVillalba/EjerciciosListado01UD05/src/ejercicio03/Principal.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Principal {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Scanner sc=new Scanner(System.in);
 
@@ -29,11 +29,14 @@ public class Principal {
 
                     System.out.println("carrera: ");
                     carrera=sc.nextLine();
+
+                    fS.write(nombre + "," + posicion + "," + tiempo + "," + carrera);
+                    fS.newLine();
                 }
             }while (nombre.length()>0);
         }catch (IOException e){
-            System.out.println("Error al introducir los datos");
-        }finally{
+            System.err.println("Error al introducir los datos");
+        } finally {
             fS.close();
             System.out.println("Adiós");
         }
